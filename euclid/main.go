3eef30e94d21e1c842ff245bd559d57fc1e5e68f
prateevnik <35513910+prateevnik@users.noreply.github.com>
2018-04-04 12:17:30 -0700

    Add files via upload

--- euclid/main.go
+++ euclid/main.go
@@ -62,12 +62,12 @@ func getTargetFileNumbers(targetFileHash string) map[string]float64 {
 }
 
 //  GetEuclideanPeers sorts stuff, should return a string for display
-// evolving as of 8-10-17
-func GetEuclideanPeers(targetFile string, targetFileHash string, osType string, numberPeersPtr *int) []string {
+func GetEuclideanPeers(targetFile string, targetFileHash string, osType string, numberPeersPtr *int, fileInfoPtr *bool,
+	userSpecifiedMetadata *string, userSpecifiedExcludeMetadata *string) []string {
 
-	euclidianPeersMap := GetEuclidianPeerBinaries(targetFileHash, osType)
+	euclidianPeersMap := GetEuclidianPeerBinaries(targetFileHash, osType, fileInfoPtr, userSpecifiedMetadata, userSpecifiedExcludeMetadata)
 	if debug {
-		fmt.Println("euclidianPeersMap is: ", euclidianPeersMap)
+		fmt.Println("::GetEuclideanPeers, euclidianPeersMap is: ", euclidianPeersMap)
 	}
 
 	// begin sorting stuff, to show peers in order of closest first..
@@ -106,28 +106,35 @@ func buildUpStringArrayOfPeers(ss []keyVal, numberPeersPtr *int) []string {
 //GetEuclidianPeerBinaries returns a map for sorting, to find closest peers for target file
 // will want to sort returned map[string]float64 as per: https://stackoverflow.com/questions/18695346/how-to-sort-a-mapstringint-by-its-values
 // #################################################################
-func GetEuclidianPeerBinaries(targetFileHash string, osType string) map[string]float64 {
+func GetEuclidianPeerBinaries(targetFileHash string, osType string, fileInfoPtr *bool,
+	userSpecifiedMetadata *string, userSpecifiedExcludeMetadata *string) map[string]float64 {
 
 	mapOfTargetFileValues := getTargetFileNumbers(targetFileHash)
 	peerBinaryRows := persistence.RetrievePeerBinaryRows(osType) // HIER
 
-	var md5Hash, fileName string
+	var md5Hash, fileName, description string
 	var staticScore, codeSize, binsz, symbols, sections, libraryCount, imports, num_data_strings, system_calls, networking_calls,
 		media_calls, ui_calls, registry_calls, security_calls, crypto_calls, database_calls, unknown_calls, known_vulnerable float64
 
 	mapOfKnownFilesWithEuclideanDistances := make(map[string]float64)
 
-	//fmt.Println("ZOWIE?")
 	for peerBinaryRows.Next() {
-		// HIER: call a Go routine to compute Euclidean
+		// HIER: should spin off a Go routine to compute Euclidean
 
-		err := peerBinaryRows.Scan(&md5Hash, &fileName, &staticScore, &codeSize, &binsz, &symbols, &sections, &libraryCount, &imports, &num_data_strings,
+		err := peerBinaryRows.Scan(&md5Hash, &fileName, &description, &staticScore, &codeSize, &binsz, &symbols, &sections, &libraryCount, &imports, &num_data_strings,
 			&system_calls, &networking_calls, &media_calls, &ui_calls, &registry_calls, &security_calls, &crypto_calls, &database_calls, &unknown_calls, &known_vulnerable)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		//fmt.Println("ZING?")
+		if *fileInfoPtr {
+			if len(description) > 0 {
+				fileName = fileName + " [" + description + "]"
+			} else {
+				fileName = fileName + " [na]"
+			}
+		}
+
 		if debug {
 			if targetFileHash == md5Hash {
 				fmt.Printf("this is our TARGETFILE! target file hash is %s and current db hash is %s \n", targetFileHash, md5Hash)
@@ -155,6 +162,123 @@ func GetEuclidianPeerBinaries(targetFileHash string, osType string) map[string]f
 		euclidPercentPeerMethodsFoundInTargetFile2 := math.Pow(LogOrZero(getPercentageFileAMethodsFoundInFileB(md5Hash, targetFileHash)), 2)
 		euclidPercentTargetFileMethodsFoundInPeer2 := math.Pow(LogOrZero(getPercentageFileAMethodsFoundInFileB(targetFileHash, md5Hash)), 2) // math.Pow returns float64
 
+		// ugly FIXME... if userSpecifiedMetadata was passed in, limit the metadata fields to those specified via flag "-metadata="
+		// for efficiency, don't bother figuring this out unless -filemetadata arg was used (non zero length)
+		if len(*userSpecifiedMetadata) > 0 {
+			// TODO parse *userSpecifiedMetadata now..
+			if !(strings.Contains(*userSpecifiedMetadata, "codeSize")) { //fmt.Println("CHECK: looks like 'codeSize' NOT within ", *userSpecifiedMetadata)
+				euclidcodeSize2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "binsz")) {
+				euclidbinsz2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "symbols")) {
+				euclidsymbols2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "sections")) {
+				euclidsections2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "libraryCount")) {
+				euclidlibraryCount2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "imports")) {
+				euclidimports2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "num_data_strings")) {
+				euclidnum_data_strings2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "system_calls")) {
+				euclidsystem_calls2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "networking_calls")) {
+				euclidnetworking_calls2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "media_calls")) {
+				euclidmedia_calls2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "ui_calls")) {
+				euclidui_calls2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "registry_calls")) {
+				euclidregistry_calls2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "security_calls")) {
+				euclidsecurity_calls2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "crypto_calls")) {
+				euclidcrypto_calls2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "database_calls")) {
+				eucliddatabase_calls2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "unknown_calls")) {
+				euclidunknown_calls2 = 0
+			}
+			if !(strings.Contains(*userSpecifiedMetadata, "shared_lib_functions")) {
+				euclidPercentPeerMethodsFoundInTargetFile2 = 0
+				euclidPercentTargetFileMethodsFoundInPeer2 = 0
+			}
+		}
+
+		// ugly, FIXME if userSpecifiedMetadata was passed in, limit the metadata fields to those specified via flag "-metadata="
+		// for efficiency, don't bother figuring this out unless -filemetadata arg was used (non zero length)
+		// This is inverse logic to the include block above.  If user DOES specify a metadata element, this time, it's to remove it
+		// from scope, so if we find that an element has been specified, we zero out that element now.
+		if len(*userSpecifiedExcludeMetadata) > 0 {
+			if strings.Contains(*userSpecifiedExcludeMetadata, "codeSize") { //fmt.Println("CHECK: looks like 'codeSize' within ", *userSpecifiedExcludeMetadata) so zero it
+				euclidcodeSize2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "binsz") {
+				euclidbinsz2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "symbols") {
+				euclidsymbols2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "sections") {
+				euclidsections2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "libraryCount") {
+				euclidlibraryCount2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "imports") {
+				euclidimports2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "num_data_strings") {
+				euclidnum_data_strings2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "system_calls") {
+				euclidsystem_calls2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "networking_calls") {
+				euclidnetworking_calls2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "media_calls") {
+				euclidmedia_calls2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "ui_calls") {
+				euclidui_calls2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "registry_calls") {
+				euclidregistry_calls2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "security_calls") {
+				euclidsecurity_calls2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "crypto_calls") {
+				euclidcrypto_calls2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "database_calls") {
+				eucliddatabase_calls2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "unknown_calls") {
+				euclidunknown_calls2 = 0
+			}
+			if strings.Contains(*userSpecifiedExcludeMetadata, "shared_lib_functions") {
+				euclidPercentPeerMethodsFoundInTargetFile2 = 0
+				euclidPercentTargetFileMethodsFoundInPeer2 = 0
+			}
+		}
+
 		euclidSumSQRT := math.Sqrt(euclidcodeSize2 + euclidbinsz2 + euclidsymbols2 + euclidsections2 + euclidlibraryCount2 + euclidimports2 + euclidnum_data_strings2 +
 			euclidsystem_calls2 + euclidnetworking_calls2 + euclidmedia_calls2 + euclidui_calls2 + euclidregistry_calls2 + euclidsecurity_calls2 + euclidcrypto_calls2 +
 			eucliddatabase_calls2 +
@@ -175,6 +299,15 @@ func GetEuclidianPeerBinaries(targetFileHash string, osType string) map[string]f
 
 	return mapOfKnownFilesWithEuclideanDistances
 }
+
+/*
+func zeroOutUnspecifiedMetadataColumns(userSpecifiedMetadata *string, float64, float64, float64, float64,
+	float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64 )
+ (float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64, float64)
+ {
+
+ }
+*/
 
 func getPercentageFileAMethodsFoundInFileB(targetFileHash string, md5Hash string) float64 {
 	// get string like "msvcrt.dll__exit;msvcrt.dll__c_exit;msvcrt.dll_strncpy", then parse it into [] string
